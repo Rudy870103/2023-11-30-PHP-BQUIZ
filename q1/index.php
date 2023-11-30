fea24b6e3d1feefab528267e3621b6c96df49f4c
rudy870103 <favoriteinfinite@gmail.com>
2023-11-30 11:53:24 +0800

    建立網站標題管理頁面

---
+++ q1/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/css/bootstrap.css">
+    <title>題組一</title>
+</head>
+
+<body>
+    <header class="container">
+        <img src="" alt="">
+    </header>
+
+    <main class="container">
+        <h3 class="text-center">網站標題管理</h3>
+        <hr>
+
+        <form action="" method="post">
+            <table class="table table-bordered text-center">
+                <tr>
+                    <td>網站標題</td>
+                    <td>替代文字</td>
+                    <td>顯示</td>
+                    <td>刪除</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><img src="" alt="" style="width:300px;height:30px"></td>
+                    <td><input type="text" name="" id="" style="width: 90%;height:30px"></td>
+                    <td><input type="radio" name="" id=""></td>
+                    <td><input type="checkbox" name="" id=""></td>
+                    <td><input class="btn btn-success" type="button" value="更新圖片"></td>
+                </tr>
+            </table>
+
+            <div class="d-flex justify-content-between">
+                <div><input type="button" value="新增網站標題圖片"></div>
+                <div><input type="submit" value="修改確定"><input type="reset" value="重置"></div>
+                <div></div>
+            </div>
+        </form>
+    </main>
+
+
+
+
+
+
+
+
+    <script src="./css/js/bootstrap.js"></script>
+</body>
+
+</html>
