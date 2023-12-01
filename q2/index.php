d103841272231ffc337b90eda6632550286ee96d
rudy870103 <favoriteinfinite@gmail.com>
2023-12-01 13:43:36 +0800

    建立前台投票列表頁面

--- q2/index.php
+++ q2/index.php
@@ -1,3 +1,4 @@
+<?php include_once "db.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,25 +14,37 @@
         <h1 class="text-center">問卷調查</h1>
     </header>
     <main class="container"></main>
-<fieldset>
-    <legend>目前位置 : 首頁 > 問卷調查</legend>
-    <table class="table">
-        <tr>
-            <th>編號</th>
-            <th>問卷題目</th>
-            <th>投票總數</th>
-            <th>結果</th>
-            <th>狀態</th>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
-</fieldset>
+    <fieldset>
+        <legend>目前位置 : 首頁 > 問卷調查</legend>
+        <div class="col-9 mx-auto">
+            <table class="table">
+            <tr>
+                <th>編號</th>
+                <th>問卷題目</th>
+                <th>投票總數</th>
+                <th>結果</th>
+                <th>狀態</th>
+            </tr>
+            <?php
+            $ques = $Que->all(['subject_id' => 0]);
+            foreach ($ques as $idx => $que) {
+            ?>
+                <tr>
+                    <td><?=$idx+1;?></td>
+                    <td><?=$que['text'];?></td>
+                    <td><?=$que['count'];?></td>
+                    <td><a href="result.php?id=<?=$que['id'];?>" class="btn btn-success">投票結果</a></td>
+                    <td>
+                        <a href="vote.php?=id=<?=$que['id'];?>" class="btn btn-danger">我要投票</a>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+        </div>
+        
+    </fieldset>
 
 
 
